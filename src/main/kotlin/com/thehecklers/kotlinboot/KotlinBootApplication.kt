d9c31e7263fe29df4117488929eea767a73145ef
Mark Heckler <mark.heckler@gmail.com>
2021-10-30 13:43:46 -0500

    Fixed dup airport in sample data, polish

--- src/main/kotlin/com/thehecklers/kotlinboot/KotlinBootApplication.kt
+++ src/main/kotlin/com/thehecklers/kotlinboot/KotlinBootApplication.kt
@@ -6,26 +6,18 @@ import org.springframework.boot.runApplication
 import org.springframework.context.annotation.Bean
 import org.springframework.data.annotation.Id
 import org.springframework.data.mongodb.core.mapping.Document
-import org.springframework.data.repository.CrudRepository
 import org.springframework.data.repository.reactive.ReactiveCrudRepository
-import org.springframework.http.MediaType
-import org.springframework.http.MediaType.*
+import org.springframework.http.MediaType.APPLICATION_JSON
+import org.springframework.http.MediaType.TEXT_EVENT_STREAM
 import org.springframework.stereotype.Service
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.PathVariable
-import org.springframework.web.bind.annotation.RestController
 import org.springframework.web.reactive.function.client.WebClient
-import org.springframework.web.reactive.function.client.awaitBody
 import org.springframework.web.reactive.function.client.bodyToMono
 import org.springframework.web.reactive.function.server.ServerRequest
-import org.springframework.web.reactive.function.server.ServerResponse
-import org.springframework.web.reactive.function.server.ServerResponse.*
+import org.springframework.web.reactive.function.server.ServerResponse.ok
 import org.springframework.web.reactive.function.server.body
 import org.springframework.web.reactive.function.server.router
 import reactor.core.publisher.Flux
-import reactor.core.publisher.Mono
 import java.time.Duration
-import java.util.List
 
 @SpringBootApplication
 class KotlinBootApplication {
@@ -40,7 +32,6 @@ class KotlinBootApplication {
                     Airport("KGAG", "Gage Airport"),
                     Airport("KLOL", "Derby Field"),
                     Airport("KSUX", "Sioux Gateway/Brig General Bud Day Field"),
-                    Airport("KLOL", "Derby Field"),
                     Airport("KBUM", "Butler Memorial Airport")
                 )
             )
@@ -67,18 +58,6 @@ fun main(args: Array<String>) {
     runApplication<KotlinBootApplication>(*args)
 }
 
-//@RestController
-class MetarController(private val service: WxService) {
-//    @GetMapping
-//    fun getAllAirports() = service.getAllAirports()
-//
-//    @GetMapping("/{id}")
-//    fun getAirportById(@PathVariable id: String) = service.getAirportById(id)
-//
-//    @GetMapping("/metar/{id}", produces = [MediaType.TEXT_EVENT_STREAM_VALUE])
-//    fun getMetarForAirport(@PathVariable id: String): Flux<METAR> = service.getMetarsForAirportById(id)
-}
-
 @Service
 class WxService(private val repo: AirportRepository, private val client: WebClient) {
     fun getAllAirports(req: ServerRequest) = ok().body<Airport>(repo.findAll())
@@ -87,16 +66,14 @@ class WxService(private val repo: AirportRepository, private val client: WebClie
 
     fun getMetarsForAirportById(req: ServerRequest) = ok()
         .contentType(TEXT_EVENT_STREAM)
-        .body<METAR>(
-            Flux.interval(Duration.ofSeconds(1))
-                .flatMap {
-                    client.get()
-                        //.uri("?loc=$id", req.pathVariable("id"))
-                        .uri("?loc=${req.pathVariable("id")}")
-                        .retrieve()
-                        .bodyToMono<METAR>()
-                        .defaultIfEmpty(METAR("???", "METAR unavailable for this airport code"))
-                })
+        .body<METAR>(Flux.interval(Duration.ofSeconds(1))
+            .flatMap {
+                client.get()
+                    .uri("?loc=${req.pathVariable("id")}")
+                    .retrieve()
+                    .bodyToMono<METAR>()
+                    .defaultIfEmpty(METAR("???", "METAR unavailable for this airport code"))
+            })
 }
 
 interface AirportRepository : ReactiveCrudRepository<Airport, String>
