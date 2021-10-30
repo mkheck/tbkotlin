05a966b9d16432fe7d10de22e312d454f3119469
Mark Heckler <mark.heckler@gmail.com>
2021-10-29 21:04:54 -0500

    Reactor, RouterDSL, handle case where METAR requested for airport that doesn't have METARs

--- build.gradle.kts
+++ build.gradle.kts
@@ -17,20 +17,18 @@ repositories {
 }
 
 dependencies {
-//	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
-	implementation("org.springframework.boot:spring-boot-starter-data-mongodb")
-//	implementation("org.springframework.boot:spring-boot-starter-data-mongodb-reactive")
-	implementation("org.springframework.boot:spring-boot-starter-web")
+//	implementation("org.springframework.boot:spring-boot-starter-data-mongodb")
+	implementation("org.springframework.boot:spring-boot-starter-data-mongodb-reactive")
+//	implementation("org.springframework.boot:spring-boot-starter-web")
 	implementation("org.springframework.boot:spring-boot-starter-webflux")
 	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
 	implementation("io.projectreactor.kotlin:reactor-kotlin-extensions")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
 //	implementation("org.jetbrains.kotlinx:kotlinx-coroutines-reactor")
-//	runtimeOnly("com.h2database:h2")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 	implementation("de.flapdoodle.embed:de.flapdoodle.embed.mongo")
-//	testImplementation("io.projectreactor:reactor-test")
+	testImplementation("io.projectreactor:reactor-test")
 }
 
 tasks.withType<KotlinCompile> {
--- src/main/kotlin/com/thehecklers/kotlinboot/KotlinBootApplication.kt
+++ src/main/kotlin/com/thehecklers/kotlinboot/KotlinBootApplication.kt
@@ -7,7 +7,9 @@ import org.springframework.context.annotation.Bean
 import org.springframework.data.annotation.Id
 import org.springframework.data.mongodb.core.mapping.Document
 import org.springframework.data.repository.CrudRepository
+import org.springframework.data.repository.reactive.ReactiveCrudRepository
 import org.springframework.http.MediaType
+import org.springframework.http.MediaType.*
 import org.springframework.stereotype.Service
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
@@ -15,7 +17,13 @@ import org.springframework.web.bind.annotation.RestController
 import org.springframework.web.reactive.function.client.WebClient
 import org.springframework.web.reactive.function.client.awaitBody
 import org.springframework.web.reactive.function.client.bodyToMono
+import org.springframework.web.reactive.function.server.ServerRequest
+import org.springframework.web.reactive.function.server.ServerResponse
+import org.springframework.web.reactive.function.server.ServerResponse.*
+import org.springframework.web.reactive.function.server.body
+import org.springframework.web.reactive.function.server.router
 import reactor.core.publisher.Flux
+import reactor.core.publisher.Mono
 import java.time.Duration
 import java.util.List
 
@@ -23,56 +31,75 @@ import java.util.List
 class KotlinBootApplication {
     @Bean
     fun clr(repo: AirportRepository) = CommandLineRunner {
-        repo.saveAll(
-            List.of(
-                Airport("KSTL", "St. Louis Lambert International Airport"),
-                Airport("KORD", "Chicago O'Hare International Airport"),
-                Airport("KFAT", "Fresno Yosemite Airport"),
-                Airport("KGAG", "Gage Airport"),
-                Airport("KLOL", "Derby Field"),
-                Airport("KSUX", "Sioux Gateway/Brig General Bud Day Field"),
-                Airport("KLOL", "Derby Field"),
-                Airport("KBUM", "Butler Memorial Airport")
+        repo.deleteAll()
+            .thenMany(
+                Flux.just(
+                    Airport("KSTL", "St. Louis Lambert International Airport"),
+                    Airport("KORD", "Chicago O'Hare International Airport"),
+                    Airport("KFAT", "Fresno Yosemite Airport"),
+                    Airport("KGAG", "Gage Airport"),
+                    Airport("KLOL", "Derby Field"),
+                    Airport("KSUX", "Sioux Gateway/Brig General Bud Day Field"),
+                    Airport("KLOL", "Derby Field"),
+                    Airport("KBUM", "Butler Memorial Airport")
+                )
             )
-        )
+            .flatMap { repo.save(it) }
+            .subscribe()
     }
 
     @Bean
     fun client() = WebClient.create("http://localhost:9876/metar")
+
+    @Bean
+    fun routerFunction(svc: WxService) = router {
+        accept(APPLICATION_JSON).nest {
+            GET("/", svc::getAllAirports)
+            GET("/{id}", svc::getAirportById)
+        }
+        accept(TEXT_EVENT_STREAM).nest {
+            GET("/metar/{id}", svc::getMetarsForAirportById)
+        }
+    }
 }
 
 fun main(args: Array<String>) {
     runApplication<KotlinBootApplication>(*args)
 }
 
-@RestController
+//@RestController
 class MetarController(private val service: WxService) {
-    @GetMapping
-    fun getAllAirports() = service.getAllAirports()
-
-    @GetMapping("/{id}")
-    fun getAirportById(@PathVariable id: String) = service.getAirportById(id)
-
-    @GetMapping("/metar/{id}", produces = [MediaType.TEXT_EVENT_STREAM_VALUE])
-    fun getMetarForAirport(@PathVariable id: String): Flux<METAR> = service.getMetarsForAirportById(id)
+//    @GetMapping
+//    fun getAllAirports() = service.getAllAirports()
+//
+//    @GetMapping("/{id}")
+//    fun getAirportById(@PathVariable id: String) = service.getAirportById(id)
+//
+//    @GetMapping("/metar/{id}", produces = [MediaType.TEXT_EVENT_STREAM_VALUE])
+//    fun getMetarForAirport(@PathVariable id: String): Flux<METAR> = service.getMetarsForAirportById(id)
 }
 
 @Service
 class WxService(private val repo: AirportRepository, private val client: WebClient) {
-    fun getAllAirports() =  repo.findAll()
+    fun getAllAirports(req: ServerRequest) = ok().body<Airport>(repo.findAll())
 
-    fun getAirportById(id: String) = repo.findById(id)
+    fun getAirportById(req: ServerRequest) = ok().body<Airport>(repo.findById(req.pathVariable("id")))
 
-    fun getMetarsForAirportById(id: String) = Flux.interval(Duration.ofSeconds(1))
-        .flatMap {
-            client.get()
-                .uri("?loc=$id")
-                .retrieve()
-                .bodyToMono<METAR>()
-        }
+    fun getMetarsForAirportById(req: ServerRequest) = ok()
+        .contentType(TEXT_EVENT_STREAM)
+        .body<METAR>(
+            Flux.interval(Duration.ofSeconds(1))
+                .flatMap {
+                    client.get()
+                        //.uri("?loc=$id", req.pathVariable("id"))
+                        .uri("?loc=${req.pathVariable("id")}")
+                        .retrieve()
+                        .bodyToMono<METAR>()
+                        .defaultIfEmpty(METAR("???", "METAR unavailable for this airport code"))
+                })
 }
 
-interface AirportRepository : CrudRepository<Airport, String>
+interface AirportRepository : ReactiveCrudRepository<Airport, String>
 
 @Document
 data class Airport(@Id val id: String, val name: String)
